a6643f8de1102433444114383ecff2bfc2825b2e
sudkak <pavanrayalu@Pavans-MacBook-Pro-2.local>
2019-04-14 16:56:44 -0400

    added item tracker component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ItemTrackerComponent } from './item-tracker/item-tracker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemTrackerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/item-tracker/item-tracker.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-item-tracker',
+  templateUrl: './item-tracker.component.html',
+  styleUrls: ['./item-tracker.component.css']
+})
+export class ItemTrackerComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
